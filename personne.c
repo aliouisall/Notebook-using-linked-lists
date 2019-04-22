db455ed82669c8dc2f8e82919866eb36cd6fd62f
momo <diopmamadou706@gmail.com>
2019-04-22 18:39:28 +0000

    link of future list

---
+++ personne.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+
+typedef struct Personne
+	{
+		char *nom;
+		char *telephone;
+		struct Personne *suiv;
+		
+	}personne; 
+
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
