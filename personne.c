5bbfc8961f9c0d29012594d892389e2eabd4d504
momo <diopmamadou706@gmail.com>
2019-04-24 19:21:03 +0000

    Adding the Create Person function

--- personne.c
+++ personne.c
@@ -8,8 +8,16 @@ typedef struct Personne
 		char *telephone;
 		struct Personne *suiv;
 		
-	}personne; 
+	}personne, *PTR; 
 
+PTR CreerPersonne(char *nom, char *telephone, PTR suiv)
+{
+	PTR n = (PTR)malloc(sizeof(personne));
+	n->nom = (char *)malloc(strlen(nom) + 1);
+	n->telephone = (char*)malloc(strlen(telephone) + 1);
+	n->suiv = suiv;
+	return n;
+}
 
 
 int main(int argc, char const *argv[])
