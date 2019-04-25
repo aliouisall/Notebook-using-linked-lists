085079721d2a9b6f25b9804105b084ae8a1a40fd
ibnibrahim <aliouibnibrahim@gmail.com>
2019-04-25 02:48:13 +0000

    Fonction d'affichage du carnet

--- main.c
+++ main.c
@@ -2,14 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define nom_max 32
-#define num_max 16
-
 //Structure personne
 typedef struct Personne Personne;
 struct Personne{
-    char nom[nom_max];
-    char numTel[num_max];
+    char *nom;
+    char *numTel;
     Personne *suivant;
 };
 
@@ -21,14 +18,14 @@ struct Carnet{
 
 //Fonction qui creer une personne
 
-PTR CreerPersonne(char *nom, char *telephone, PTR suiv)
-{
-    PTR n = (PTR)malloc(sizeof(personne));
-    n->nom = (char *)malloc(strlen(nom) + 1);
-    n->telephone = (char*)malloc(strlen(telephone) + 1);
-    n->suiv = suiv;
-    return n;
-}
+// PTR CreerPersonne(char *nom, char *telephone, PTR suiv)
+// {
+//     PTR n = (PTR)malloc(sizeof(personne));
+//     n->nom = (char *)malloc(strlen(nom) + 1);
+//     n->telephone = (char*)malloc(strlen(telephone) + 1);
+//     n->suiv = suiv;
+//     return n;
+// }
 
 // Fonction d'initialisation du carnet
 Carnet *initialisation()
@@ -47,6 +44,24 @@ Carnet *initialisation()
     carnet->premier = personne;
 
     return carnet;
+}
+
+// Fonction d'affichage du carnet
+void afficherCarnet(Carnet *carnet){
+    
+    if (carnet == NULL){
+        exit(EXIT_FAILURE);
+    }
+
+    Personne *actuel = carnet->premier;
+    int i = 1;
+
+    while (actuel != NULL){
+        printf("Position - %d | Nom : %s et Numéro de téléphone : %s \n", i, actuel->numTel, actuel->nom);
+        actuel = actuel->suivant;
+    }
+    printf("NULL\n");
+
 }
 
 int main(int argc, char const *argv[])
